ccb0e3ba528d648ebf6ac8c93017f5d983cb7df0
ankybhagat <ankita.bhagat1@gmail.com>
2016-03-08 15:48:12 -0600

    commit

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var app = express();
-var cool = require('cool-ascii-faces');
-var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,33 +10,72 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  var result = ''
-  var times = process.env.TIMES || 5
-  for (i=0; i < times; i++)
-    result += cool();
-  response.send(result);
+  response.render('pages/index');
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+app.get('/country', function(request, response) {
+	var country= [
+  {
+    "code": "AD",
+    "name": "Andorra",
+    "population": "84000"
+  },
+  {
+    "code": "AE",
+    "name": "United Arab Emirates",
+    "population": "4975593"
+  },
+  {
+    "code": "AF",
+    "name": "Afghanistan",
+    "population": "29121286"
+  },
+  {
+    "code": "AG",
+    "name": "Antigua and Barbuda",
+    "population": "86754"
+  },
+  {
+    "code": "AI",
+    "name": "Anguilla",
+    "population": "13254"
+  },
+  {
+    "code": "AL",
+    "name": "Albania",
+    "population": "2986952"
+  },
+  {
+    "code": "AM",
+    "name": "Armenia",
+    "population": "2968000"
+  },
+  {
+    "code": "AO",
+    "name": "Angola",
+    "population": "13068161"
+  },
+  {
+    "code": "AM",
+    "name": "Armenia",
+    "population": "2968000"
+  },
+  {
+    "code": "AO",
+    "name": "Angola",
+    "population": "13068161"
+  },
+  {
+    "code": "AQ",
+    "name": "Antarctica",
+    "population": "0"
+  }
+  ]
+ 
+  response.setHeader('Content-Type', 'application/json');
+    response.send(JSON.stringify(country));
 });
-
-
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-})
-
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
