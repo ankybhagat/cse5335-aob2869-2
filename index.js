a863489c987ca9aeb4ed799c96607c614e8c6196
ankybhagat <ankita.bhagat1@gmail.com>
2016-04-26 18:31:00 -0500

    DS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
-var con="https://postgres://zfqdzrffxhdbgw:TvM7zTFns7nctywAEFi1_8MXVw@ec2-54-197-254-213.compute-1.amazonaws.com:5432/dapsioq4fqt59o";
+var con="postgres://zfqdzrffxhdbgw:TvM7zTFns7nctywAEFi1_8MXVw@ec2-54-197-254-213.compute-1.amazonaws.com:5432/dapsioq4fqt59o";
 // var con="postgres://postgres:140392@localhost/postgres";
 
 app.set('port', (process.env.PORT || 5000));
