6933979dce1f1e19440c34c3291053593444a1f2
ankybhagat <ankita.bhagat1@gmail.com>
2016-04-26 09:37:26 -0500

    Ds

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ app.get('/db', function (request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.render('pages/db', {results: result.rows} ); }
+        {
+          var result.rows = JSON.stringify(results);
+         response.render('pages/db', {results: result.rows} ); }
     });
   
   });
